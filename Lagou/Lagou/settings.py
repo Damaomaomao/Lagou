4287d4b6a1224afd7a042729d637fb25873eec40
Damaomaomao <40875511+Damaomaomao@users.noreply.github.com>
2018-07-27 18:01:31 +0800

    Update settings.py

--- Lagou/Lagou/settings.py
+++ Lagou/Lagou/settings.py
@@ -8,7 +8,7 @@
 #     https://doc.scrapy.org/en/latest/topics/settings.html
 #     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
-import random
+
 BOT_NAME = 'Lagou'
 
 SPIDER_MODULES = ['Lagou.spiders']
@@ -40,7 +40,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = random.randint(0,3)
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
